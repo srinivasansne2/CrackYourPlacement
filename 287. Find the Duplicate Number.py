6b6471e734e9abb023f1f1457c000a834eed175f
srinivasansne2 <srinivasansne2@gmail.com>
2024-07-21 00:30:39 +0530

    comit

--- 287. Find the Duplicate Number.py
+++ 287. Find the Duplicate Number.py
@@ -1,9 +1,9 @@
 class Solution(object):
     def findDuplicate(self, nums):
-        arr = {}
-        for i in nums:
-            if i in arr:
-                return i
-            else:
-                arr[i] = 1
-        return 0
+        seen = {}        # A set to put the numbers we already seen
+        for i in nums:     # Take all the elements of the list one by one
+            if i in seen:   # If we already seen this element
+                return i   # It is the repeated number !
+            else:          # We haven't seen this number yet
+                seen[i] = 1 # So we put it in the seen set
+        return 0           # Here, we have no duplicate number, so returning 0
