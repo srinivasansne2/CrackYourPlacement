254b9ae2e6b679eddd765f6a3d7773f8e518acee
srinivasansne2 <srinivasansne2@gmail.com>
2024-07-22 12:25:15 +0530

    revice

---
+++ 1. Two Sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen = {}
+        for i, v in enumerate(nums):
+            remaining = target - v
+            if remaining in seen:
+                return [seen[remaining], i]
+            seen[v] = i
+        return []
