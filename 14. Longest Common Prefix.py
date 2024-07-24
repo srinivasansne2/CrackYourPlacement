65533b0bdce532cfdc1db3a1db55a8f92fd0e7a7
srinivasansne2 <srinivasansne2@gmail.com>
2024-07-25 01:01:12 +0530

    Neet to revise

---
+++ 14. Longest Common Prefix.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        p=strs[0]
+        for s in strs[1:]:
+            while not s.startswith(p):
+                p=p[:-1]
+                if not p:
+                    return ""
+        return p
