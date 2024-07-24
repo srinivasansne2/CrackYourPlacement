0c3aeb62aab39afccb0cbe7c274019801b8c53c9
srinivasansne2 <srinivasansne2@gmail.com>
2024-07-24 23:34:52 +0530

    need to learn

---
+++ 20. Valid Parentheses.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isValid(self, s):
+        brackets = {'{':'}','(':')','[':']'}
+        stack = []
+        for char in s:
+            if char in brackets:
+                stack.append(char)
+            elif not stack or brackets[stack.pop()] != char:
+                return False
+        return not stack
+            
