5e3097e4dec9ff0255507b02eceeb44c5dfc8bf5
srinivasansne2 <srinivasansne2@gmail.com>
2024-07-29 00:37:13 +0530

    3 Need Time

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -1,11 +1,12 @@
 class Solution:
-    def isValid(self, s):
-        brackets = {'{':'}','(':')','[':']'}
+    def isValid(self, s: str) -> bool:
+        bracket_map = {')': '(', '}': '{', ']': '['}
         stack = []
         for char in s:
-            if char in brackets:
+            if char in bracket_map:
+                top_element = stack.pop() if stack else '#'
+                if bracket_map[char] != top_element:
+                    return False
+            else:
                 stack.append(char)
-            elif not stack or brackets[stack.pop()] != char:
-                return False
         return not stack
-    
