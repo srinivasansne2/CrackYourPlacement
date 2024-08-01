c8ee90e30dde060c6b05f2e515d7a75fc6c724cf
srinivasansne2 <srinivasansne2@gmail.com>
2024-08-01 23:23:24 +0530

    Completed

--- 88. Merge Sorted Array.py
+++ 88. Merge Sorted Array.py
@@ -1,5 +1,5 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
-        for num in range(n):
-            nums1[m+num] = nums2[num]
-        nums1.sort()
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        for num in range(n):           #Creatong for loop for len of nums2
+            nums1[m+num] = nums2[num]  #Then We Replasing The nums1 values using this
+        nums1.sort()                   #Then We Sorting nums1
