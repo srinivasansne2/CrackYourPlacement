0aac03d9b70d51974182c3e441bd8fe1559f6015
srinivasansne2 <srinivasansne2@gmail.com>
2024-08-08 23:33:09 +0530

    meed to

---
+++ 289. Game of Life.py
@@ -0,0 +1,27 @@
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        rows = len(board)
+        cols = len(board[0])
+        directions = [[0, 1], [1, 0], [0, -1], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]
+        
+        liveCount = 0
+        toChange = {}
+
+        def valid(r, c):
+            return (0 <= r < rows) and (0 <= c < cols)
+
+        for r in range(rows):
+            for c in range(cols):
+                liveCount = 0
+                for dr, dc in directions:
+                    newRow, newCol = r + dr, c + dc
+                    if valid(newRow, newCol) and board[newRow][newCol] == 1:
+                        liveCount += 1
+                if board[r][c] == 1:
+                    if liveCount < 2 or liveCount > 3:
+                        toChange[(r, c)] = 0 
+                else:
+                    if liveCount == 3:
+                        toChange[(r, c)] = 1  
+        for (r, c), nextState in toChange.items():
+            board[r][c] = nextState
