146cf4ba8d4319cb094f3c2dd256597661b1de26
srinivasansne2 <srinivasansne2@gmail.com>
2024-08-20 23:45:57 +0530

    Pending

---
+++ 65. Valid Number.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        import re
+        integer = r"[-+]?\d+"
+        decimal = r"[-+]?(?=\.?\d)\d*\.\d*"
+        return re.compile("(%s|%s)([eE]%s)?" % (integer, decimal, integer)).fullmatch(s)
